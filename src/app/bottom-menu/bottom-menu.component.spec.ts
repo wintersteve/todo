7b166fe85f4d3d5ee6aa64a53157477f38e725ae
Steve Winter <steve.winter@diva-e.com>
2021-08-17 19:14:38 +0200

    chore(angular): upgrade Angular to version 11

--- src/app/bottom-menu/bottom-menu.component.spec.ts
+++ src/app/bottom-menu/bottom-menu.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BottomMenuComponent } from './bottom-menu.component';
 
@@ -6,7 +6,7 @@ describe('BottomMenuComponent', () => {
   let component: BottomMenuComponent;
   let fixture: ComponentFixture<BottomMenuComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BottomMenuComponent ]
     })

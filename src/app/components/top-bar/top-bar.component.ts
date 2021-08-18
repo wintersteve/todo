6703bf12f93e9e6911ba32f8b967342b769581ac
Steve Winter <steve.winter@diva-e.com>
2021-08-18 23:39:30 +0200

    feat(component): add user information

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth/auth.service';
 
 @Component({
-  selector: 'app-top-bar',
-  templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.scss']
+	selector: 'app-top-bar',
+	templateUrl: './top-bar.component.html',
+	styleUrls: ['./top-bar.component.scss'],
 })
-export class TopBarComponent implements OnInit {
+export class TopBarComponent {
+	public readonly user$ = this.authService.getUser();
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	constructor(private readonly authService: AuthService) {}
 }

ab08350a2399be9d415080f4d9949b4c366f3da0
Steve Winter <steve.winter@diva-e.com>
2021-08-19 19:40:56 +0200

    feat(routing): add login route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 import { MainComponent } from './pages/main/main.component';
 
 const routes: Routes = [
 	{
 		path: '',
 		component: MainComponent,
+	},
+	{
+		path: 'login',
+		component: LoginComponent,
 	},
 	{
 		path: '**',

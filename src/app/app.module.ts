ee1091cab62d670ce0fddb4ef4c457ffb4ee7892
Steve Winter <steve.winter@diva-e.com>
2021-09-02 08:22:30 +0200

    feat(config): provide http interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { PagesModule } from './pages/pages.module';
 import { NetlifyIdentityModule } from './libs/netlify-identity/netlify-identity.module';
+import { AuthInterceptor } from './shared/interceptors/auth.interceptor';
 
 @NgModule({
 	declarations: [AppComponent],
@@ -24,5 +25,12 @@ import { NetlifyIdentityModule } from './libs/netlify-identity/netlify-identity.
 		}),
 	],
 	bootstrap: [AppComponent],
+	providers: [
+		{
+			provide: HTTP_INTERCEPTORS,
+			useClass: AuthInterceptor,
+			multi: true,
+		},
+	],
 })
 export class AppModule {}

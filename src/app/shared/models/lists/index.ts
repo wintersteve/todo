738293ae18caa556defc4948f6d663cf4c796f9b
Steve Winter <steve.winter@diva-e.com>
2021-09-06 09:21:14 +0200

    feat(models): implement default list enum

--- src/app/shared/models/lists/index.ts
+++ src/app/shared/models/lists/index.ts
@@ -1,3 +1,10 @@
+export enum DEFAULT_LIST {
+	INBOX = '307737360342188232',
+	TODAY = '307737452442812608',
+	URGENT = '307737471526895816',
+	DONE = '307737510811795655',
+}
+
 export interface List {
 	icon: string;
 	id: string;

91bac71affdd4a6be159bdb9dcc029075eeb2a05
Steve Winter <steve.winter@diva-e.com>
2021-09-07 09:57:14 +0200

    refactor(models): extract models in a separate file

--- serverless/functions/lists-get.ts
+++ serverless/functions/lists-get.ts
@@ -2,22 +2,7 @@ import { Handler } from '@netlify/functions';
 import { fragments } from './utils/fragments';
 import { client } from './utils/gql';
 import { getTokenFromRequest, getUserId } from './utils/auth';
-
-interface List {
-	icon: string;
-	id: string;
-	isCustom: boolean;
-	title: string;
-}
-
-interface FindListsData {
-	findDefaultLists: {
-		data: List[];
-	};
-	findListsByUser: {
-		data: List[];
-	};
-}
+import { FindListsData } from './models';
 
 const query = `
   ${fragments.ListFields.definition}
--- serverless/functions/models/index.ts
+++ serverless/functions/models/index.ts
@@ -1,5 +1,14 @@
-interface Variables {
+import { List } from 'src/app/shared/models/lists';
+
+export interface Variables {
 	[key: string]: Variables | string;
 }
 
-export { Variables };
+export interface FindListsData {
+	findDefaultLists: {
+		data: List[];
+	};
+	findListsByUser: {
+		data: List[];
+	};
+}
